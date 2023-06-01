56c378768b3766a2e00fa883bd7f7fe34c27c4d1
jbisits <jbisits@gmail.com>
2023-06-01 19:26:51 +1000

    [no ci] Try Gaussian grid stretch (fail)

--- scripts/cabbeling_withparameterisation.jl
+++ scripts/cabbeling_withparameterisation.jl
@@ -2,7 +2,7 @@
 
 using DirectNumericalShenanigans
 
-resolution = (Nx = 10, Ny = 10 , Nz = 1090)
+resolution = (Nx = 10, Ny = 10 , Nz = 1000)
 diffusivities = (ν = 1e-6, κ = (S = 1e-9, T = 1e-7))
 ## Initial conditions, interface in middle of domain
 S₀ = (upper = 34.568, lower = 34.7)
@@ -14,41 +14,12 @@ model.grid
 scatterlines(zspacings(model.grid, Center()), znodes(model.grid, Center()))
 set_two_layer_initial_conditions!(model, S₀, Θ₀)
 
-lines(-tanh.(range(-1, 0; length = 100)), range(-1, -3/4; length = 100))
-h(k) = (k - 1) / resolution.Nz
-function grid_stretching(k)
-    ϵ = 1e-5 #mm
-    find_upper = findall((k .- 1) ./ resolution.Nz .≤ 0.25)
-    upper = range(0.25, 0; length = length(find_upper))
-    upper_faces = tanh.(upper)
-    find_lower = findall((k .- 1) ./ resolution.Nz .≥ 0.75)
-    lower = range(0, 0.25; length = length(find_lower))
-    lower_faces = tanh.(lower)
-    find_middle = findall(0.25 .< (k .- 1) ./ resolution.Nz .< 0.75)
-    middle_faces = zeros(length(find_middle))
-    zfaces = vcat(upper_faces, middle_faces, lower_faces) .+ ϵ
-
-    z = Vector{Float64}(undef, resolution.Nz + 1)
-    z[1] = 0
-    for i ∈ eachindex(z)
-
-    end
-    return zfaces
-end
-
-test_zfaces = range(1, length = resolution.Nz)
-grid_stretching(test_zfaces)
-scatterlines(grid_stretching(test_zfaces), test_zfaces)
-zfaces = vcat(0, grid_stretching(test_zfaces))
-## Still experimenting with grid stretching
-chebychev_spaced_z_faces(k) = -1/2 -1/2 * cos(π * (k - 1) / resolution.Nz)
-σ = 1
-hyperbolically_spaced_faces(k) =  -1 * (1 - tanh(σ * (k - 1) / resolution.Nz) / tanh(σ))
+gaussian_spacing(k) = exp(-((k - 1)/ resolution.Nz + 0.5)^2/2) - 1
 grid = RectilinearGrid(topology = (Periodic, Periodic, Bounded),
                         size = (resolution.Nx, resolution.Ny, resolution.Nz),
                         x = (-0.5/2, 0.5/2),
                         y = (-0.5/2, 0.5/2),
-                        z = chebychev_spaced_z_faces)
+                        z = gaussian_spacing)
 scatterlines(zspacings(grid, Center()), znodes(grid, Center()))
 
 ## This will be new way to set initial conditions with tanh

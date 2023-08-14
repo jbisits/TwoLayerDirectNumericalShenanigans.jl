6efe0f36e3eccac0cbbcf20790f2099efefbf8c5
jbisits <jbisits@gmail.com>
2023-08-14 15:16:02 +1000

    Fixes

--- examples/high_resolution.jl
+++ examples/high_resolution.jl
@@ -17,9 +17,8 @@ cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ.cabbeling, T₀ᵘ)
 unstable = UnstableUpperLayerInitialConditions(S₀ᵘ.unstable, T₀ᵘ)
 isohaline = IsohalineUpperLayerInitialConditions(T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(unstable)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 50)
-set_two_layer_initial_conditions!(model, initial_conditions, profile_function;
-                                  salinity_perturbation = true)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 50.0)
+set_two_layer_initial_conditions!(model, initial_conditions, profile_function)
 add_velocity_random_noise!(model, 1e-2, INTERFACE_LOCATION)
 
 ## build the simulation
